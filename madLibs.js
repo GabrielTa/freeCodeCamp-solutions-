7b246decb217df86fe0ca7d15c9df45ad6f8a747
Gabriel <gtamavicius@gmail.com>
2017-05-02 15:18:20 +0200

    second commit

---
+++ madLibs.js
@@ -0,0 +1,14 @@
+
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  var result = "";
+  // Your code below this line
+  result= "In other news my " + myAdjective + " " + myNoun + " " + myVerb + " "+ "and went on a rampage " + myAdverb + " eating the neighborhoods cats!";
+  console.log (result);
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
+
